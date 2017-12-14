e817d3b89fb8b949e61eca2a42cbf78899992119
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-12-14 23:16:39 +0100

    Change js

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,8 @@ $(function() {
 	var width = 400;
 	var animationSpeed = 1000;
 	var pause = 3000;
-
 	var $carousel = $('#carousel ul');
 
-	var moveSlide;
 
 	setInterval (function() {
 		var firstItem = $carousel.find("li:first");
@@ -24,13 +22,23 @@ $(function() {
 		});
 	}, pause);
 	
-	// $('#previous_slide').click(function() {
+	$('#previous_slide').click(function() {
+		var firstItem = $carousel.find("li:first");
+		var lastItem = $carousel.find("li:last");
+		$carousel.animate({'margin-left': '+='+width}, animationSpeed, function() {
+			firstItem.before(lastItem)
+			$carousel.css({marginLeft:-400});
+		});
+	});
 
-	// 	moveSlide(setInterval);
+	$('#next_slide').click(function() {
+		var firstItem = $carousel.find("li:first");
+		var lastItem = $carousel.find("li:last");
+		$carousel.animate({'margin-left': '-='+width}, animationSpeed, function() {
+			lastItem.after(firstItem);
+			$carousel.css({marginLeft:0});
+		});
+	});
 
-	// });
-
-	// $('#next_slide').click(function() {
-	// });
 
 });
